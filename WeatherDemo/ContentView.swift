d312b3b7a48b7bce7e0e4849b75f39ffef877669
Timo Erkkilä <timo.erkkila@sasken.com>
2019-11-29 00:59:46 +0200

    Initial Commit

---
+++ WeatherDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherDemo
+//
+//  Created by Timo Erkkilä on 29.11.2019.
+//  Copyright © 2019 Timo Erkkilä. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
